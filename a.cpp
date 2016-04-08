ae2b4e1248914bad62686d2c8b160aafae4acd61
bumjoonjo <sisobus1@gmail.com>
2016-04-08 21:26:56 +0900

    complete

--- a.cpp
+++ a.cpp
@@ -11,9 +11,9 @@
 #include <algorithm>
 using namespace std;
 //#define DEBUG
-#ifndef DEBUG
-#define RELEASE
-#endif
+//#define UNIX_ENVIRONMENT
+#define SHOW_SCREEN
+#define SHOW_GRAMMER_USED
 
 typedef pair<string,vector<string> > svs;
 /*
@@ -29,18 +29,19 @@ struct Grammer {
     }
     void printG() {
         for ( map<string,vector<string> >::iterator it=g.begin();it!=g.end();it++ ) {
-            printf("%s -> ",it->first.c_str());
-            for ( int i = 0 ; i < (int)it->second.size() ; i++ ) 
-                printf("%s ",it->second[i].c_str());
-            puts("");
+            fprintf(stdout,"%s -> ",it->first.c_str());
+            for ( int i = 0 ; i < (int)it->second.size() ; i++ ) {
+                fprintf(stdout,"%s ",it->second[i].c_str());
+            }
+            fprintf(stdout,"\n");
         }
     }
     void printBg() {
         for ( multimap<vector<string>,string>::iterator it=bg.begin();it!=bg.end();it++ ) {
             for ( int i = 0 ; i < (int)it->first.size() ; i++ ) 
-                printf("%s ",it->first[i].c_str());
-            printf("-> ");
-            printf("%s\n",it->second.c_str());
+                fprintf(stdout,"%s ",it->first[i].c_str());
+            fprintf(stdout,"-> ");
+            fprintf(stdout,"%s\n",it->second.c_str());
         }
     }
 };
@@ -71,14 +72,13 @@ Grammer initGrammer(string grammerFilename) {
  * create cky table
  */
 struct Cky {
-    vector<vector<set<string> > > table;
+    vector<vector<set<pair<string,string> > > > table;
     Cky(){}
     Cky(int n) {
         for ( int i = 0 ; i < n ; i++ ) {
-            vector<set<string> > cur;
+            vector<set<pair<string,string> > > cur;
             for ( int j = 0 ; j < n ; j++ ) {
-                set<string> curS;
-                curS.clear();
+                set<pair<string,string> > curS;
                 cur.push_back(curS);
             }
             table.push_back(cur);
@@ -87,12 +87,12 @@ struct Cky {
     void print() {
         for ( int i = 0 ; i < (int)table.size() ; i++ ) {
             for ( int j = 0 ; j < (int)table[i].size() ; j++ ) {
-                printf("[");
-                for ( set<string>::iterator it=table[i][j].begin();it!=table[i][j].end();it++ ) 
-                    printf("%s%s",it==table[i][j].begin()?"":",",(*it).c_str());
-                printf("]");
+                fprintf(stdout,"[");
+                for ( set<pair<string,string> >::iterator it=table[i][j].begin();it!=table[i][j].end();it++ ) 
+                    fprintf(stdout,"%s%s",it==table[i][j].begin()?"":",",(*it).first.c_str());
+                fprintf(stdout,"]");
             }
-            puts("");
+            fprintf(stdout,"\n");
         }
     }
 };
@@ -114,23 +114,31 @@ vector<vector<string> > initInputString(string inputFilename) {
     fclose(fp);
     return ret;
 }
-void printParsedInputString(vector<string>& parsedInputString) {
+void printParsedInputString(FILE* fp,vector<string>& parsedInputString) {
     for ( int i = 0 ; i < (int)parsedInputString.size() ; i++ ) 
-        printf("%s ",parsedInputString[i].c_str());
-    puts("");
+        fprintf(fp,"%s ",parsedInputString[i].c_str());
+    fprintf(fp,"\n");
 }
-void selfParsing(Grammer& grammer,Cky& cky,int i,int j) {
+void selfParsing(FILE* fp,Grammer& grammer,Cky& cky,int i,int j) {
     pair<multimap<vector<string>,string>::iterator,multimap<vector<string>,string>::iterator> foundGrammer;
-    set<string> copy = cky.table[i][j];
-    for ( set<string>::iterator it=copy.begin();it!=copy.end();it++ ) {
+    set<pair<string,string> > copy = cky.table[i][j];
+    for ( set<pair<string,string> >::iterator it=copy.begin();it!=copy.end();it++ ) {
         vector<string> cur;
-        cur.push_back(*it);
+        cur.push_back(it->first);
         foundGrammer = grammer.bg.equal_range(cur);
-        for ( multimap<vector<string>,string>::iterator it=foundGrammer.first;it!=foundGrammer.second; it++ ) 
-            cky.table[i][j].insert(it->second);
+        for ( multimap<vector<string>,string>::iterator it1=foundGrammer.first;it1!=foundGrammer.second; it1++ ) {
+#ifdef SHOW_GRAMMER_USED
+            fprintf(fp,"%s -> %s\n",it1->second.c_str(),it->first.c_str());
+#ifdef SHOW_SCREEN
+            fprintf(stdout,"%s -> %s\n",it1->second.c_str(),it->first.c_str());
+#endif
+#endif
+            string addedClass = "("+it1->second+" "+it->second+")";
+            cky.table[i][j].insert(pair<string,string>(it1->second,addedClass));
+        }
     }
 }
-Cky calculateCky(Grammer& grammer,vector<string>& parsedInputString) {
+Cky calculateCky(FILE *fp,Grammer& grammer,vector<string>& parsedInputString) {
     Cky cky((int)parsedInputString.size());    
     
     pair<multimap<vector<string>,string>::iterator,multimap<vector<string>,string>::iterator> foundGrammer;
@@ -139,7 +147,14 @@ Cky calculateCky(Grammer& grammer,vector<string>& parsedInputString) {
         cur.push_back(parsedInputString[i]);
         foundGrammer = grammer.bg.equal_range(cur);
         for ( multimap<vector<string>,string>::iterator it=foundGrammer.first;it!=foundGrammer.second;it++ ) {
-            cky.table[0][i].insert(it->second);
+#ifdef SHOW_GRAMMER_USED
+            fprintf(fp,"%s -> %s\n",it->second.c_str(),parsedInputString[i].c_str());
+#ifdef SHOW_SCREEN
+            fprintf(stdout,"%s -> %s\n",it->second.c_str(),parsedInputString[i].c_str());
+#endif
+#endif
+            string addedClass = "("+it->second+" "+parsedInputString[i]+")";
+            cky.table[0][i].insert(pair<string,string>(it->second,addedClass));
         }
     }
     int n = (int)parsedInputString.size();
@@ -147,20 +162,28 @@ Cky calculateCky(Grammer& grammer,vector<string>& parsedInputString) {
         for ( int j = 0 ; j <= n-l ; j++ ) {
             for ( int i = l-2 ; i >= 0 ; i-- ) {
                 /* self parsing */
-                selfParsing(grammer,cky,i,j);
-                selfParsing(grammer,cky,l-i-2,j+i+1);
+                selfParsing(fp,grammer,cky,i,j);
+                selfParsing(fp,grammer,cky,l-i-2,j+i+1);
                 /* self parsing end */
 
-                set<string> lhs = cky.table[i][j];
-                set<string> rhs = cky.table[l-i-2][j+i+1];
-                for ( set<string>::iterator it1=lhs.begin();it1!=lhs.end();it1++ ) {
-                    for ( set<string>::iterator it2=rhs.begin();it2!=rhs.end();it2++ ) {
+                set<pair<string,string> > lhs = cky.table[i][j];
+                set<pair<string,string> > rhs = cky.table[l-i-2][j+i+1];
+                for ( set<pair<string,string> >::iterator it1=lhs.begin();it1!=lhs.end();it1++ ) {
+                    for ( set<pair<string,string> >::iterator it2=rhs.begin();it2!=rhs.end();it2++ ) {
                         vector<string> cur;
-                        cur.push_back(*it1);
-                        cur.push_back(*it2);
+                        cur.push_back(it1->first);
+                        cur.push_back(it2->first);
                         foundGrammer = grammer.bg.equal_range(cur);
-                        for ( multimap<vector<string>,string>::iterator it=foundGrammer.first;it!=foundGrammer.second; it++ ) 
-                            cky.table[l-1][j].insert(it->second);
+                        for ( multimap<vector<string>,string>::iterator it=foundGrammer.first;it!=foundGrammer.second; it++ ) {
+#ifdef SHOW_GRAMMER_USED
+                            fprintf(fp,"%s -> %s %s\n",it->second.c_str(),it1->first.c_str(),it2->first.c_str());
+#ifdef SHOW_SCREEN
+                            fprintf(stdout,"%s -> %s %s\n",it->second.c_str(),it1->first.c_str(),it2->first.c_str());
+#endif
+#endif
+                            string addedClass = "("+it->second+" "+it1->second+it2->second+")";
+                            cky.table[l-1][j].insert(pair<string,string>(it->second,addedClass));
+                        }
                     }
                 }
             }
@@ -168,9 +191,30 @@ Cky calculateCky(Grammer& grammer,vector<string>& parsedInputString) {
     }
     return cky;
 }
+void printParseTree(FILE* fp,Cky& cky,vector<string>& parsedInputString) {
+    int n = (int)parsedInputString.size();
+    for ( int i = 0 ; i < n ; i++ ) {
+        if ( cky.table[n-1][i].empty() ) continue;
+        set<pair<string,string> > ans = cky.table[n-1][i];
+        for ( set<pair<string,string> >::iterator it=ans.begin();it!=ans.end();it++ ) {
+            if ( it->first != "S" ) continue;
+            fprintf(fp,"%s\n",it->second.c_str());
+        }
+    }
+}
 int main(int argc,char *argv[]) {
+    /*
+     *  default filename
+     */
     string grammerFilename = "grammer.txt";
     string inputFilename = "input.txt";
+    string outputFilename = "output.txt";
+    /*
+     *  you can change default filename to argument 
+     *  argv[1] : grammer filename
+     *  argv[2] : input filename
+     */
+#ifdef UNIX_ENVIRONMENT
     if ( argc == 3 ) {
         char buf[1024];
         sscanf(argv[1],"%s",buf);
@@ -178,6 +222,13 @@ int main(int argc,char *argv[]) {
         sscanf(argv[2],"%s",buf);
         inputFilename = buf;
     }
+#endif
+    FILE* fp = fopen(outputFilename.c_str(),"w");
+
+    /*
+     *  grammer initializing
+     *  input string parsing
+     */
     Grammer grammer = initGrammer(grammerFilename);
     vector<vector<string> > parsedInputString = initInputString(inputFilename);
 #ifdef DEBUG
@@ -185,7 +236,26 @@ int main(int argc,char *argv[]) {
     grammer.printBg();
     printParsedInputString(parsedInputString[0]);
 #endif
-    Cky cky = calculateCky(grammer,parsedInputString[0]);
-    cky.print();
+    for ( int i = 0 ; i < (int)parsedInputString.size() ; i++ ) {
+        fprintf(fp,"/*\n * cky parser start\n * input string : ");
+        printParsedInputString(fp,parsedInputString[i]);
+        fprintf(fp," */\n");
+#ifdef SHOW_SCREEN
+        fprintf(stdout,"/*\n * cky parser start\n * input string : ");
+        printParsedInputString(stdout,parsedInputString[i]);
+        fprintf(stdout," */\n");
+#endif
+        Cky cky = calculateCky(fp,grammer,parsedInputString[i]);
+#ifdef SHOW_SCREEN
+#endif
+#ifdef DEBUG
+        cky.print(fp);
+#endif
+        printParseTree(fp,cky,parsedInputString[i]);
+#ifdef SHOW_SCREEN
+        printParseTree(stdout,cky,parsedInputString[i]);
+#endif
+    }
+    fclose(fp);
     return 0;
 }
